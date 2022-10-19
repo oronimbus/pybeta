d16e001e88c0a1e347ca16dde2db4e037f5ef2f0
oronimbus <claudio.fontana@hotmail.ch>
2022-10-19 19:45:48 +0100

    add forecast combination

--- src/beta.py
+++ src/beta.py
@@ -29,7 +29,7 @@ class Beta:
 
     def _weighted_ols(
         self, X: np.array, y: np.array, w: np.array = None, demean: bool = False
-        ) -> np.array:
+    ) -> np.array:
         """Helper class to calculate beta using WLS.
 
         Args:
@@ -40,19 +40,19 @@ class Beta:
 
         Returns:
             np.array: vector of betas
-        """        
+        """
         if demean:
             X -= np.mean(X, axis=0)
-        
+
         if w is None:
             w = np.ones(X.shape[0])
         w = np.diag(w)
         return np.linalg.inv(X.T @ w @ X) @ X.T @ w @ y
-    
+
     def ols(self, adjusted: bool = False) -> float:
         """Classic beta calculation using OLS.
-        
-        Beta can be shrunk towards unity using Merril Lynch approach. This is the same 
+
+        Beta can be shrunk towards unity using Merril Lynch approach. This is the same
         as Blume (1975).
 
         Args:
@@ -65,8 +65,7 @@ class Beta:
         if adjusted:
             return 0.67 * beta + 0.33
         return beta
-        
-    
+
     def vasicek(self, beta_prior: float = 1, se_prior: float = 0.5) -> float:
         """Bayesian estimation of beta using Vasicek (1973).
 
@@ -82,20 +81,19 @@ class Beta:
         s_yy = np.sum(np.square(self.endog - endog_hat)) / (self.n_obs - 2)
         s_xx = np.sum(np.square(self.exog - np.mean(self.exog)))
         std_error = np.sqrt(s_yy / s_xx)
-        
-        # Bayesian estimation of marginal posterior  
+
+        # Bayesian estimation of marginal posterior
         num = beta_prior / np.square(se_prior) + beta[1] / np.square(std_error)
         den = 1 / np.square(se_prior) + 1 / np.square(std_error)
         return np.ravel(num / den)[0]
-        
-    
+
     def welch(self, delta: float = 3, rho: float = 0) -> float:
         """Slope winsorized beta using Welch (2021).
 
         A decay factor ``rho`` can be chosen such that more relevance is given
         to more recent observation. The paper uses ``2/256`` as an exponential
         decay factor.
-    
+
         Args:
             delta (float, optional): winsorisation parameter. Defaults to 3.
             rho (float, optional): decay factor. Defaults to 0.
@@ -109,15 +107,16 @@ class Beta:
         weights = np.exp(-rho * np.arange(self.n_obs)[::-1])
         beta = self._weighted_ols(self.exog_mat, endog_wins, w=weights)
         return np.ravel(beta)[1]
-        
+
     def robeco(
         self, corr_target: float, vol_target: float, gamma: float = 0.5, phi: float = 0.2
-        ) -> float:
+    ) -> float:
         """Beta shrinkage using Blitz et al. (2022).
 
         The Robeco beta is calculated using the correlation of asset to market returns and the
         ratio of their volatilties separately. Both are shrunk towards a cross-sectional mean.
-        
+        The beta is the product of the correlation and the volatility ratio.
+
         Args:
             corr_target (float): cross sectional mean of correlation
             vol_target (float): cross sectional mean of volatility ratio
@@ -125,25 +124,86 @@ class Beta:
             phi (float, optional): volatility ratio shrinkage factor. Defaults to 0.2.
 
         Returns:
-            float: _description_
+            float: Robeco beta
         """
-        corr = np.corrcoef(self.exog.T, self.endog.T)[0,1]
+        corr = np.corrcoef(self.exog.T, self.endog.T)[0, 1]
         corr_shrink = (1 - gamma) * corr + gamma * corr_target
         vol_ratio = np.std(self.endog) / np.std(self.exog)
         vol_shrink = (1 - phi) * vol_ratio + phi * vol_target
-        beta = corr_shrink / vol_shrink
+        beta = corr_shrink * vol_shrink
         return beta
 
     def scholes_williams(self) -> float:
         """Calculate shrunk beta using Scholes & Williams (1977)."""
-        beta_lag = np.ravel(self._weighted_ols(self.exog_mat[1:,:], self.endog[:-1,:]))[-1]
-        beta_lead = np.ravel(self._weighted_ols(self.exog_mat[:-1,:], self.endog[1:,:]))[-1]
+        beta_lag = np.ravel(self._weighted_ols(self.exog_mat[1:, :], self.endog[:-1, :]))[-1]
+        beta_lead = np.ravel(self._weighted_ols(self.exog_mat[:-1, :], self.endog[1:, :]))[-1]
         beta = np.ravel(self._weighted_ols(self.exog_mat, self.endog))[-1]
-        auto_corr = np.corrcoef(self.exog[1:,:].T, self.exog[:-1,:].T)[0,1]
-        
+        auto_corr = np.corrcoef(self.exog[1:, :].T, self.exog[:-1, :].T)[0, 1]
+
         beta = (beta_lag + beta + beta_lead) / (1 + 2 * auto_corr)
         return beta
-        
-    def bayesian_model_avg(self):
+
+
+class BetaForecastCombination:
+    def __init__(self, asset_returns: np.array, benchmark_returns: np.array, window: int = 21):
+        self.endog = np.atleast_2d(asset_returns).T
+        self.exog = np.atleast_2d(benchmark_returns).T
+        self.window = window
+
+        # define parameters
+        self.n_obs = self.endog.shape[0]
+        self.weights = None
+
+    def _generate_estimation_windows(self, data):
+        return [data[: self.window + i] for i in range(data.shape[0] - self.window)]
+
+    def _generate_betas(self, windows, **kwargs):
+        # set up iterator
+        beta_obj = [Beta(i[:, 0], i[:, 1]) for i in windows]
+        c, v = kwargs.get("corr_target", 0.5), kwargs.get("vol_target", 2)
+
+        # consumer iterator and cast into 2d numpy array
+        ols = np.atleast_2d(list(map(lambda x: x.ols(), beta_obj))).T
+        adj_ols = np.atleast_2d(list(map(lambda x: x.ols(True), beta_obj))).T
+        vasicek = np.atleast_2d(list(map(lambda x: x.vasicek(), beta_obj))).T
+        welch = np.atleast_2d(list(map(lambda x: x.welch(), beta_obj))).T
+        aged_welch = np.atleast_2d(list(map(lambda x: x.welch(rho=2 / 256), beta_obj))).T
+        robeco = np.atleast_2d(list(map(lambda x: x.robeco(c, v), beta_obj))).T
+        # schol_will = np.atleast_2d(list(map(lambda x: x.scholes_williams(), beta_obj))).T
+
+        return np.hstack([ols, adj_ols, vasicek, welch, aged_welch, robeco])
+
+    def fit(self):
+        # split training data from test data for parameter estimation
+        cutoff = self.n_obs - self.window
+        train_data = np.hstack([self.exog[:cutoff, :], self.endog[:cutoff, :]])
+        test_data = np.hstack([self.exog[cutoff:, :], self.endog[cutoff:, :]])
+
+        # calculate beta using expanding window
+        training_windows = self._generate_estimation_windows(train_data)
+        betas = self._generate_betas(training_windows)
+
+        # regress betas onto realised betas
+        one = np.ones([train_data.shape[0] - self.window - 1, 1])
+        X = np.hstack([one, betas[:-1, :]])
+        self.weights = np.linalg.pinv(X) @ betas[1:, 0]
+
+        # project weights onto test data
+        betas_test = self._generate_betas([test_data])
+        X_test = np.hstack([np.ones((betas_test.shape[0], 1)), betas_test])
+        return np.ravel(X_test @ self.weights)[0]
+
+
+class BetaBMA(BetaForecastCombination):
+    def __init__(
+        self,
+        asset_returns: np.array,
+        benchmark_returns: np.array,
+        window: float = 21,
+        shrinkage: float = None,
+    ):
+        super().__init__(asset_returns, benchmark_returns, window)
+        self.shrinkage = shrinkage
+
+    def fit(self):
         pass
-        
---
+++ src/utils.py
@@ -0,0 +1,15 @@
+"""Feature utility functions for data processing."""
+import pandas as pd
+
+
+def stack_dict_to_df(_dict: dict):
+    """Stack dict of dicts into dataframe."""
+    stacked = []
+    for k, v in _dict.items():
+        df = pd.DataFrame.from_dict(v)
+        df["index"] = k
+        index_name = "level_0" if df.index.name is None else df.index.name
+        df = df.reset_index().set_index(["index", index_name])
+        df.index.names = ("", "")
+        stacked.append(df)
+    return pd.concat(stacked).T
