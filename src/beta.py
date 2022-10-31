8c96b967b9117a2a7bb04150ac1d2b1baf79d7aa
oronimbus <claudio.fontana@hotmail.ch>
2022-10-31 22:26:32 +0000

    add beta version of bayesian model averaging

--- src/beta.py
+++ src/beta.py
@@ -17,7 +17,13 @@ References:
     Vasicek, Oldrich A. 'A Note on Using Cross-Sectional Information in Bayesian Estimation of
         Security Betas'. The Journal of Finance 28, no. 5 (December 1973): 1233-39.
 """
+from itertools import chain, combinations
 import numpy as np
+
+
+def add_intercept(data):
+    """Add column vector of ones to front of matrix."""
+    return np.hstack([np.ones((data.shape[0], 1)), data])
 
 
 class Beta:
@@ -135,29 +141,27 @@ class Beta:
 
     def scholes_williams(self) -> float:
         """Calculate shrunk beta using Scholes & Williams (1977)."""
-        beta_lag = np.ravel(self._weighted_ols(self.exog_mat[1:, :], self.endog[:-1, :]))[-1]
-        beta_lead = np.ravel(self._weighted_ols(self.exog_mat[:-1, :], self.endog[1:, :]))[-1]
+        beta_lead = np.ravel(self._weighted_ols(self.exog_mat[1:, :], self.endog[:-1, :]))[-1]
+        beta_lag = np.ravel(self._weighted_ols(self.exog_mat[:-1, :], self.endog[1:, :]))[-1]
         beta = np.ravel(self._weighted_ols(self.exog_mat, self.endog))[-1]
-        auto_corr = np.corrcoef(self.exog[1:, :].T, self.exog[:-1, :].T)[0, 1]
+        auto_corr = np.corrcoef(self.exog[1:, :], self.exog[:-1, :], rowvar=False)[0, 1]
 
         beta = (beta_lag + beta + beta_lead) / (1 + 2 * auto_corr)
         return beta
 
 
 class BetaForecastCombination:
-    def __init__(self, asset_returns: np.array, benchmark_returns: np.array, window: int = 21):
-        self.endog = np.atleast_2d(asset_returns).T
-        self.exog = np.atleast_2d(benchmark_returns).T
+    def __init__(self, exog: np.array, endog: np.array, window: int = 21):
+        self.exog = np.atleast_2d(exog).T
+        self.endog = np.atleast_2d(endog).T
         self.window = window
-
-        # define parameters
         self.n_obs = self.endog.shape[0]
         self.weights = None
 
-    def _generate_estimation_windows(self, data):
+    def _generate_estimation_windows(self, data: np.array) -> list:
         return [data[: self.window + i] for i in range(data.shape[0] - self.window)]
 
-    def _generate_betas(self, windows, **kwargs):
+    def _generate_betas(self, windows: list, **kwargs: dict) -> np.array:
         # set up iterator
         beta_obj = [Beta(i[:, 0], i[:, 1]) for i in windows]
         c, v = kwargs.get("corr_target", 0.5), kwargs.get("vol_target", 2)
@@ -173,7 +177,7 @@ class BetaForecastCombination:
 
         return np.hstack([ols, adj_ols, vasicek, welch, aged_welch, robeco])
 
-    def fit(self):
+    def fit(self) -> float:
         # split training data from test data for parameter estimation
         cutoff = self.n_obs - self.window
         train_data = np.hstack([self.exog[:cutoff, :], self.endog[:cutoff, :]])
@@ -185,25 +189,68 @@ class BetaForecastCombination:
 
         # regress betas onto realised betas
         one = np.ones([train_data.shape[0] - self.window - 1, 1])
-        X = np.hstack([one, betas[:-1, :]])
-        self.weights = np.linalg.pinv(X) @ betas[1:, 0]
+        X_train = add_intercept(betas[:-1, :])
+        self.weights = np.linalg.pinv(X_train) @ betas[1:, 0]
 
         # project weights onto test data
         betas_test = self._generate_betas([test_data])
-        X_test = np.hstack([np.ones((betas_test.shape[0], 1)), betas_test])
+        X_test = add_intercept(betas_test)
         return np.ravel(X_test @ self.weights)[0]
 
 
 class BetaBMA(BetaForecastCombination):
-    def __init__(
-        self,
-        asset_returns: np.array,
-        benchmark_returns: np.array,
-        window: float = 21,
-        shrinkage: float = None,
-    ):
-        super().__init__(asset_returns, benchmark_returns, window)
+    def __init__(self, exog: np.array, endog: np.array, window: int = 21, shrinkage: float = None):
+        super().__init__(exog, endog)
         self.shrinkage = shrinkage
+        
+    def _generate_beta_combinations(self, data):
+        indices = list(range(data.shape[0]))
+        combos = [list(combinations(indices, i)) for i in range(1, data.shape[0])]
+        return list(chain(*combos))  
+            
+    def fit(self, dof_r: int = 1):
+        # estimation windows for prior
+        data_matrix = np.hstack([self.exog, self.endog])
+        training_windows = self._generate_estimation_windows(data_matrix)
+        beta_w = self._generate_betas(training_windows)
+        beta_s = add_intercept(self._generate_betas([data_matrix]))
 
-    def fit(self):
-        pass
+        # retrieve all beta estimates and possible combinations
+        combos = self._generate_beta_combinations(beta_w[0])
+        
+        # set up model inputs
+        g = 1 / min(len(training_windows), len(combos))
+        a_g = g / (1 + g)
+        
+        # step 1: restricted model
+        beta_r = add_intercept(beta_w[:-dof_r, [0]])
+        b_r_hat = np.linalg.pinv(beta_r) @ beta_w[dof_r:, [0]]
+        y_r_hat = beta_r @ b_r_hat
+        ssr_r = np.sum(np.square(beta_w[dof_r:, [0]] - y_r_hat))
+        
+        # step 2: iterate over beta combinations, store as list of tuples
+        models = []
+        for combo in combos:
+            # calculate SSR_u first using lagged realised betas
+            beta_u = add_intercept(beta_w[:-1, combo])
+            b_u_hat = np.linalg.pinv(beta_u) @ beta_w[1:, [0]]
+            y_u_hat = beta_u @ b_u_hat
+            ssr_u = np.sum(np.square(beta_w[1:, [0]] - y_u_hat))
+            
+            # project combined beta onto series of realised betas
+            weights = np.zeros((beta_w.shape[1] + 1, 1))
+            weights[0, :] = b_u_hat[0,:]
+            weights[np.array(combo) + 1, :] = b_u_hat[1:,:]
+            beta_k = np.ravel(beta_s @ weights)[0]
+            models.append((len(combo), beta_k, ssr_u))
+            
+        # step 3: calculate beta weights
+        w_k = []
+        for p, _, ssr in models:
+            w_k.append([np.power(a_g, 0.5 * p) * np.power(1 + 1 / g * ssr / ssr_r, -0.5 * dof_r)])
+        w_final = np.array([w / np.sum(w_k) for w in w_k])
+        
+        # step 4: combine weights with betas for final estimate
+        beta_bma = np.sum(np.array(models)[:,1] * w_final)
+            
+        return beta_bma
